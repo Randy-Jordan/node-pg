3dfbf9e78a193123c10f0ba5ce6d9b0d258d95f6
Randy <randyjordan@email.com>
2024-05-11 19:20:24 -0500

    Node globals

--- eslint.config.js
+++ eslint.config.js
@@ -9,7 +9,7 @@ export default [
                 ecmaVersion: 2022,
                 sourceType: "module",
                 globals: {
-                    ...globals.browser,
+                    ...globals.node,
                    
                 }
             },
